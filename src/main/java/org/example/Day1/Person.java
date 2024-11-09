b0be6ce8eb09980c3331a6e4f480cf20a9b4eecc
Min Pyae Hein <minpyaehein536h@prestigein.com>
2024-11-09 11:50:18 +0700

    first commit

---
+++ src/main/java/org/example/Day1/Person.java
@@ -0,0 +1,8 @@
+package org.example.Day1;
+
+public class Person {
+    public String name;
+    public int age;
+    public String address;
+    public String city;
+}
---
+++ src/main/java/org/example/Day2/ForLopping.java
@@ -0,0 +1,9 @@
+package org.example.Day2;
+
+public class ForLopping {
+    public static void main(String[] args) {
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Hello World");
+        }
+    }
+}
